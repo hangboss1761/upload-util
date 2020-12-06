e47f3670775fec6f5c1860a7165b2bdf6cbee8e6
hangboss1761 <1240123692@qq.com>
2020-12-06 23:54:39 +0800

    test: 完善jest配置

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
+  setupFilesAfterEnv: ['./jest.setup.js'],
   rootDir: __dirname,
   testMatch: ['<rootDir>/test/**/*test.[jt]s?(x)'],
   coverageDirectory: 'coverage',
